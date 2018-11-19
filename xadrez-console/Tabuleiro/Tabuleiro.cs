a54f88b0b2884549ee619fa26e3a52a00be11f7e
Marcelo Cutin <marcelo.cutin@gmail.com>
2018-11-19 21:19:04 -0200

    Fazendo os primeiros testes de movimentação

--- xadrez-console/Tabuleiro/Tabuleiro.cs
+++ xadrez-console/Tabuleiro/Tabuleiro.cs
@@ -38,6 +38,18 @@
             p.posicao = pos;
         }
 
+        public Peca retirarPeca(Posicao pos)
+        {
+            if (peca(pos) == null)
+            {
+                return null;
+            }
+            Peca aux = peca(pos);
+            aux.posicao = null; // Peça retirada do tabuleiro
+            pecas[pos.linha, pos.coluna] = null; // Marca posição do tabuleiro como vazia
+            return aux;
+        }
+
         public bool posicaoValida(Posicao pos)
         {
             if(pos.linha<0 || pos.linha>=linhas || pos.coluna<0 || pos.coluna >= colunas)
